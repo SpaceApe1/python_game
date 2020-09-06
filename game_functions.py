df2a14708dc9ddb6a9b0fd30e1550b188ad1d464
SpaceApe1 <spaceapes.consulting@gmail.com>
2020-09-06 20:50:29 +0200

    feat: intruduced bullets

--- game_functions.py
+++ game_functions.py
@@ -1,7 +1,8 @@
 import sys
 import pygame
+from bullet import Bullet
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """Respond to keypresses and mouse evnts."""
     # watch for keyboard and mouse events.
     for event in pygame.event.get():
@@ -9,18 +10,20 @@ def check_events(ship):
             sys.exit()
         
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Resond to keypresses."""
     if event.key == pygame.K_RIGHT:
             ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
-
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings, screen, ship, bullets)
+        
 def check_keyup_events(event, ship):
     """Resond to keypresses."""    
     if event.key == pygame.K_RIGHT:
@@ -28,10 +31,29 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def update_screen(ai_settings, screen, ship):
-    # Redraw the screen  during each pass through the loop.
-    screen.fill(ai_settings.bg_color)
-    ship.blitme()
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Fire a bullet if limit not reached yet."""
+    # Create a new bullet and add it to the bullets group.
+    if len(bullets) < ai_settings.bullet_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
+def update_bullets(bullets):
+    """Update position of bullets and get rid of old bullets."""
+    # Get bullet posoitions.
+    bullets.update()
+
+    # Get rid of bullets that have disappeared.
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+def update_screen(ai_settings, screen, ship, bullets):
+    # Redraw thescreen  during each pass through the loop.
+    screen.fill(ai_settings.bg_color)
+    # Redraw all bullets behind ship and aliens
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+    ship.blitme()
     # Make the most recently draw screen visiblae.
     pygame.display.flip()
